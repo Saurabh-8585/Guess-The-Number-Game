cf53cb501e38345302b4de671a58d2a5a87acbc5
Saurabh-8585 <99315618+Saurabh-8585@users.noreply.github.com>
2022-08-16 16:26:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let score = 1;
 document.getElementById('btn').onclick = function () {
     let userInput = document.getElementById('userInput').value;
     if (randomNumber == userInput) {
-        document.getElementById('ans').innerHTML = `<i class="fa-solid fa-circle-check"></i> You take ${score}  Chances to complete the game`;
+        document.getElementById('ans').innerHTML = `<i class="fa-solid fa-circle-check"></i> You took ${score}  Chances to complete the game`;
         document.getElementById('displayScore').innerHTML = `Your score is ${10 - score} out of 10`;
         document.getElementById('displayScore').style.color = 'green';
         document.getElementById('ans').style.color = 'green';
